d87af2ee0949d03987b80a6effaed0030d2c3281
Igor Viana <igorvianacont@gmail.com>
2025-10-28 20:29:22 -0300

    Adicionando bootstrap e configurações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DetailsComponent } from './pages/details/details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'details', component: DetailsComponent },
+];
